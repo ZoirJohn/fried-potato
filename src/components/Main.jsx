78ea1cc2dc126fe6cde4fb18ab83decd3822c5cb
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2024-07-05 13:11:18 +0500

    added lazy load

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,39 +1,44 @@
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import styles from '../css/Main.module.css';
 import ProfileContainer from './profile/ProfileContainer';
-import DialogsContainer from './dialogs/DialogsContainer';
 import UsersContainer from './users/UsersContainer';
 import Login from './login/Login';
 import News from './news/News';
 import Music from './music/Music';
 import Settings from './settings/Settings';
+import { lazy,Suspense } from 'react';
+
+const DialogsContainer = lazy(() => import('./dialogs/DialogsContainer'));
+
 
 const Main = (props) => {
 	return (<main className={styles.main}>
-		<Routes>
-			<Route path={'/'}
-				   element={<ProfileContainer/>}/>
-			<Route path={'/profile/:userId?'}
-				   element={<ProfileContainer/>}/>
+		<Suspense>
+			<Routes>
+				<Route path={'/'}
+					element={<ProfileContainer />} />
+				<Route path={'/profile/:userId?'}
+					element={<ProfileContainer />} />
 
-			<Route path="/dialogs"
-				   element={<DialogsContainer/>}/>
-			<Route path="/news"
-				   element={<News/>}/>
-			<Route path="/music"
-				   element={<Music/>}/>
-			<Route path="/settings"
-				   element={<Settings/>}/>
-			<Route path="/users"
-				   element={<UsersContainer/>}/>
+				<Route path="/dialogs"
+					element={<DialogsContainer />} />
+				<Route path="/news"
+					element={<News />} />
+				<Route path="/music"
+					element={<Music />} />
+				<Route path="/settings"
+					element={<Settings />} />
+				<Route path="/users"
+					element={<UsersContainer />} />
 
-			{/*No render is here*/}
-			<Route path="/login"
-				   element={<Login/>}/>
+				{/*No render is here*/}
+				<Route path="/login"
+					element={<Login />} />
 
 
-		</Routes>
+			</Routes>
+		</Suspense>
 
 
 	</main>);
