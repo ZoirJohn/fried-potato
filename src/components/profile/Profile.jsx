53381d299632876ec1e6d1d6e497d7e4bd684aeb
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2024-07-12 12:12:08 +0500

    corrected a mistake with status bar

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,7 +1,7 @@
 import PostModel from './PostModel';
 import background from '../../img/background.jpeg';
 import profileUser from '../../img/profile-user.webp';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from '../../css/Profile.module.css';
 import AddPostRedux from './ProfileForm';
 
@@ -22,31 +22,32 @@ const Profile = (props) => {
 	const handleWord = () => {
 		setWord(word = currentWord);
 	};
+	console.log(!edit && word);
 	return (<section className={styles.profile}>
-		<img src={background} alt="background"/>
+		<img src={background} alt="background" />
 		<div className={styles.dataBox}>
-			<img src={props.profileUser?.photos.large || profileUser} alt="background"/>
+			<img src={props.profileUser?.photos.large || profileUser} alt="background" />
 			<div className={styles.info}>
 				<p><span>Full name:</span> {props.profileUser?.fullName || 'Carnage'}</p>
-				<p><span>Status: </span>
-					<samp onDoubleClick={handleStatus}>{!edit && word}</samp>
-					<samp onDoubleClick={handleStatus}>
+				<div className={styles.statusBox}><span>Status: </span>
+					{!edit && <pre className={styles['status-word']} onDoubleClick={handleStatus}>{word}</pre>}
+					<pre onDoubleClick={handleStatus}>
 						{edit && <input value={word} onChange={(e) => {
 							handleClick(e);
 							handleWord(e);
-						}} type="text"/>}
-					</samp>
-				</p>
+						}} type="text" />}
+					</pre>
+				</div>
 				<p><span>Education:</span> Embry-Riddle</p>
 			</div>
 		</div>
 		<div className={styles.messages}>
 			<AddPostRedux onSubmit={(formData) => {
 				props.addPost(formData.AddPostForm);
-			}}/>
+			}} />
 			<ul className={styles.messagesBox}>
 				{props.posts.map((p, id) => <PostModel text={p.text} profilePhoto={props.profilePhoto}
-													   likeNumber={p.likeNumber} key={id}/>)}
+					likeNumber={p.likeNumber} key={id} />)}
 			</ul>
 		</div>
 	</section>);
