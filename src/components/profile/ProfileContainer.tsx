4f2702b1a352fb865efe9e70761b9b2e43ea496a
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2024-09-13 22:09:25 +0500

    finished profile to tsx

---
+++ src/components/profile/ProfileContainer.tsx
@@ -0,0 +1,72 @@
+import { Component } from "react"
+import { connect } from "react-redux"
+import Profile from "./Profile"
+import Loader from "../../assets/Loader"
+import { withAuthRedirect } from "../../hoc/withAuthRedirect"
+import { compose } from "redux"
+import { addPost, setProfile, setStatus, updateStatus } from "../../redux/profile-reducer"
+import { withRouter } from "../../hoc/withRouter"
+import { MessageType, ProfileType } from "../../types"
+import { rootStateType } from "../../redux/store"
+
+type MapStateToProps = {
+      posts: Array<MessageType>
+      profileUser: ProfileType
+      status: string
+      id: number
+}
+type MapDispatchToProps = {
+      addPost: (formData: string) => void
+      updateStatus: () => void
+      setProfile: (id: number) => void
+      setStatus: (id: number) => void
+}
+
+type RouterType = {
+      params: {
+            userId: number
+      }
+      location: string
+      navigate: string
+}
+
+type OwnPropsType = {
+      router: RouterType
+}
+type PropsType = MapStateToProps & OwnPropsType & MapDispatchToProps
+
+class ProfileContainer extends Component<PropsType> {
+      refreshProfile() {
+            let id = this.props.router.params.userId
+            if (!id) id = this.props.id
+            this.props.setProfile(id)
+            this.props.setStatus(id)
+      }
+      componentDidMount() {
+            this.refreshProfile()
+      }
+      shouldComponentUpdate(nextProps: PropsType) {
+            return nextProps.status === this.props.status
+      }
+      componentDidUpdate(prevProps: PropsType) {
+            if (this.props.router.params.userId != prevProps.router.params.userId) {
+                  this.refreshProfile()
+            }
+      }
+      render() {
+            if (!this.props.profileUser) {
+                  return <Loader isFetching={true} />
+            }
+            return <Profile {...this.props} />
+      }
+}
+const mapStateToProps = (state: rootStateType): MapStateToProps => {
+      return {
+            posts: state.profile.posts,
+            profileUser: state.profile.profileUser,
+            status: state.profile.status,
+            id: state.auth.id,
+      }
+}
+
+export default compose(connect(mapStateToProps, { addPost, setProfile, setStatus, updateStatus }), withRouter, withAuthRedirect)(ProfileContainer)
--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -1,14 +1,14 @@
-import { reset } from "redux-form";
-import { profileAPI } from "../api/api";
-import createThunk from "../assets/createThunk";
-import { MessageType, PhotosType, ProfileType } from "../types";
+import { reset } from "redux-form"
+import { profileAPI } from "../api/api"
+import createThunk from "../assets/createThunk"
+import { MessageType, PhotosType, ProfileType } from "../types"
 
-const ADD_POST_PROFILE = "profile/ADD-POST-PROFILE";
-const DELETE_POST_PROFILE = "profile/DELETE-POST-PROFILE";
-const SET_PROFILE = "profile/SET-PROFILE";
-const SET_STATUS = "profile/SET-STATUS";
-const SET_PROFILE_PHOTO = "profile/SET-PROFILE-PHOTO";
-const SAVE_PROFILE = "profile/SAVE-PROFILE";
+const ADD_POST_PROFILE = "profile/ADD-POST-PROFILE"
+const DELETE_POST_PROFILE = "profile/DELETE-POST-PROFILE"
+const SET_PROFILE = "profile/SET-PROFILE"
+const SET_STATUS = "profile/SET-STATUS"
+const SET_PROFILE_PHOTO = "profile/SET-PROFILE-PHOTO"
+const SAVE_PROFILE = "profile/SAVE-PROFILE"
 
 let initialState = {
       posts: [
@@ -18,57 +18,57 @@ let initialState = {
       ] as Array<MessageType>,
       profileUser: null as ProfileType | null,
       status: null as string | null,
-};
+}
 
-export type InitialStateProfileType = typeof initialState;
+export type InitialStateProfileType = typeof initialState
 
 const profile_reducer = (_state = initialState, action: any) => {
       switch (action.type) {
             case ADD_POST_PROFILE:
-                  let newPost = { text: action.text, likeNumber: Math.floor(Math.random() * 10) };
-                  return { ..._state, posts: [..._state.posts, newPost] };
+                  let newPost = { text: action.text, likeNumber: Math.floor(Math.random() * 10), id: 4 }
+                  return { ..._state, posts: [..._state.posts, newPost] }
             case DELETE_POST_PROFILE:
-                  return { ..._state, posts: _state.posts.filter((post) => post.id !== action.id) };
+                  return { ..._state, posts: _state.posts.filter((post) => post.id !== action.id) }
             case SET_PROFILE:
-                  return { ..._state, profileUser: action.profileUser };
+                  return { ..._state, profileUser: action.profileUser }
             case SET_STATUS:
-                  return { ..._state, status: action.status };
+                  return { ..._state, status: action.status }
             case SET_PROFILE_PHOTO:
-                  return { ..._state, profileUser: { ..._state.profileUser, photos: action.photo } };
+                  return { ..._state, profileUser: { ..._state.profileUser, photos: action.photo } }
             case SAVE_PROFILE:
-                  return { ..._state, profileUser: { ..._state.profileUser, ...action.data } };
+                  return { ..._state, profileUser: { ..._state.profileUser, ...action.data } }
             default:
-                  return _state;
+                  return _state
       }
-};
+}
 
 // ? Action creators
-type addPostType = { type: typeof ADD_POST_PROFILE; text: string };
-const addPost = (text: string): addPostType => ({ type: ADD_POST_PROFILE, text });
+type addPostType = { type: typeof ADD_POST_PROFILE; text: string }
+const addPost = (text: string): addPostType => ({ type: ADD_POST_PROFILE, text })
 
-type deletePostType = { type: typeof DELETE_POST_PROFILE; id: number };
-const deletePost = (id: number): deletePostType => ({ type: DELETE_POST_PROFILE, id });
+type deletePostType = { type: typeof DELETE_POST_PROFILE; id: number }
+const deletePost = (id: number): deletePostType => ({ type: DELETE_POST_PROFILE, id })
 
-type setProfileDoneType = { type: typeof SET_PROFILE; profileUser: ProfileType };
-const setProfileDone = (profileUser: ProfileType): setProfileDoneType => ({ type: SET_PROFILE, profileUser });
+type setProfileDoneType = { type: typeof SET_PROFILE; profileUser: ProfileType }
+const setProfileDone = (profileUser: ProfileType): setProfileDoneType => ({ type: SET_PROFILE, profileUser })
 
-type setStatusDoneType = { type: typeof SET_STATUS; status: string };
-const setStatusDone = (status: string): setStatusDoneType => ({ type: SET_STATUS, status });
+type setStatusDoneType = { type: typeof SET_STATUS; status: string }
+const setStatusDone = (status: string): setStatusDoneType => ({ type: SET_STATUS, status })
 
-type setProfilePhotoDoneType = { type: typeof SET_PROFILE_PHOTO; photo: PhotosType };
-const setProfilePhotoDone = (photo: PhotosType): setProfilePhotoDoneType => ({ type: SET_PROFILE_PHOTO, photo });
+type setProfilePhotoDoneType = { type: typeof SET_PROFILE_PHOTO; photo: PhotosType }
+const setProfilePhotoDone = (photo: PhotosType): setProfilePhotoDoneType => ({ type: SET_PROFILE_PHOTO, photo })
 
 // ? Thunks
-const setProfile = (userId: number) => createThunk(profileAPI.GET_PROFILE_USER, setProfileDone, userId);
-const setStatus = (userId: number) => createThunk(profileAPI.GET_PROFILE_STATUS, setStatusDone, userId);
-const savePhoto = (photo: PhotosType) => createThunk(profileAPI.UPDATE_PROFILE_PHOTO, setProfilePhotoDone, photo);
-const updateStatus = (status: string) => createThunk(profileAPI.UPDATE_PROFILE_STATUS, setStatusDone, status);
+const setProfile = (userId: number) => createThunk(profileAPI.GET_PROFILE_USER, setProfileDone, userId)
+const setStatus = (userId: number) => createThunk(profileAPI.GET_PROFILE_STATUS, setStatusDone, userId)
+const savePhoto = (photo: PhotosType) => createThunk(profileAPI.UPDATE_PROFILE_PHOTO, setProfilePhotoDone, photo)
+const updateStatus = (status: string) => createThunk(profileAPI.UPDATE_PROFILE_STATUS, setStatusDone, status)
 
 const saveProfile = (data: ProfileType) => async (dispatch: Function) => {
-      const response = await profileAPI.UPDATE_PROFILE(data);
+      const response = await profileAPI.UPDATE_PROFILE(data)
       if (response.resultCode === 0) {
-            dispatch(reset("settings_data"));
+            dispatch(reset("settings_data"))
       }
-};
+}
 
-export { profile_reducer, addPost, deletePost, setProfile, setStatus, updateStatus, savePhoto, saveProfile };
+export { profile_reducer, addPost, deletePost, setProfile, setStatus, updateStatus, savePhoto, saveProfile }
