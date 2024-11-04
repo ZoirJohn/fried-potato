922863ffeda60b880d5a955a05c95a8d0b550dbe
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2024-11-04 15:30:48 +0500

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { useEffect } from "react";
-import Loader from "./assets/Loader";
-import HeaderComponent from "./components/header/HeaderComponent";
-import Main from "./components/Main";
-import Sidebar from "./components/Sidebar";
-import { withRouter } from "./hoc/withRouter";
-import { connect } from "react-redux";
-import { compose } from "redux";
-import { initializeApp } from "./redux/app-reducer";
-import { rootStateType } from "./redux/store"
+import React, { useEffect } from 'react'
+import Loader from './assets/Loader'
+import HeaderComponent from './components/header/HeaderComponent'
+import Main from './components/Main'
+import Sidebar from './components/Sidebar'
+import { withRouter } from './hoc/withRouter'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { initializeApp } from './redux/app-reducer'
+import { rootStateType } from './redux/store'
 
 const App = (props: any) => {
       useEffect(() => {
-            props.initializeApp();
-      }, []);
+            props.initializeApp()
+      }, [])
 
       if (!props.initialized) {
-            return <Loader isFetching={props.initialized}/>;
+            return <Loader isFetching={props.initialized} />
       }
       return (
             <div data-testid='app' className='App'>
@@ -25,20 +25,20 @@ const App = (props: any) => {
                         <Sidebar />
                   </div>
             </div>
-      );
-};
+      )
+}
 
 const mapStateToProps = (state: rootStateType) => {
       return {
             initialized: state.app.initialized,
-      };
-};
+      }
+}
 
-const Wrapper = compose(
+const Wrapper = compose<React.ComponentType>(
       withRouter,
       connect(mapStateToProps, {
             initializeApp,
       })
-)(App);
+)(App)
 
-export default Wrapper;
+export default Wrapper
