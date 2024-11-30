619b6d39386b63f1c320704ac425a43f18d7e695
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2024-11-30 20:38:59 +0500

    downloaded ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Loader from './assets/Loader'
 import HeaderComponent from './components/header/HeaderComponent'
 import Main from './components/Main'
@@ -8,8 +8,16 @@ import { connect } from 'react-redux'
 import { compose } from 'redux'
 import { initializeApp } from './redux/app-reducer'
 import { rootStateType } from './redux/store'
+import { MenuFoldOutlined, MenuUnfoldOutlined, UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons'
+import { Button, Layout, Menu, theme } from 'antd'
+
+const { Header, Sider, Content } = Layout
 
 const App = (props: any) => {
+     const [collapsed, setCollapsed] = useState(false)
+     const {
+           token: { colorBgContainer, borderRadiusLG },
+     } = theme.useToken()
       useEffect(() => {
             props.initializeApp()
       }, [])
@@ -19,10 +27,62 @@ const App = (props: any) => {
       }
       return (
             <div data-testid='app' className='App'>
-                  <HeaderComponent />
+                  {/* <HeaderComponent /> */}
+                  <Layout>
+                        <Sider trigger={null} collapsible collapsed={collapsed}>
+                              <div className='demo-logo-vertical' />
+                              <Menu
+                                    theme='dark'
+                                    mode='inline'
+                                    defaultSelectedKeys={['1']}
+                                    items={[
+                                          {
+                                                key: '1',
+                                                icon: <UserOutlined />,
+                                                label: 'nav 1',
+                                          },
+                                          {
+                                                key: '2',
+                                                icon: <VideoCameraOutlined />,
+                                                label: 'nav 2',
+                                          },
+                                          {
+                                                key: '3',
+                                                icon: <UploadOutlined />,
+                                                label: 'nav 3',
+                                          },
+                                    ]}
+                              />
+                        </Sider>
+                        <Layout>
+                              <Header style={{ padding: 0, background: colorBgContainer }}>
+                                    <Button
+                                          type='text'
+                                          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                                          onClick={() => setCollapsed(!collapsed)}
+                                          style={{
+                                                fontSize: '16px',
+                                                width: 64,
+                                                height: 64,
+                                          }}
+                                    />
+                              </Header>
+                              <Content
+                                    style={{
+                                          margin: '24px 16px',
+                                          padding: 24,
+                                          minHeight: 280,
+                                          background: colorBgContainer,
+                                          borderRadius: borderRadiusLG,
+                                    }}
+                              >
+                                    Content
+                              </Content>
+                        </Layout>
+                  </Layout>
                   <div data-testid='container' className='container'>
-                        <Main />
-                        <Sidebar />
+                        {/* <Main />
+                        <Sidebar /> */}
                   </div>
             </div>
       )
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 import { Navigate } from 'react-router-dom'
-
+import { useState } from 'react'
 import styles from '../css/Main.module.css'
 import ProfileContainer from './profile/ProfileContainer'
 import Login from './login/Login'
 import { lazy, Suspense } from 'react'
+
 
 const Dialogs = lazy(() => import('./dialogs/DialogsContainer'))
 const News = lazy(() => import('./news/News'))
@@ -12,9 +13,14 @@ const Music = lazy(() => import('./music/Music'))
 const Settings = lazy(() => import('./settings/Settings'))
 const Users = lazy(() => import('./users/UsersContainer'))
 
+
+
 const Main = () => {
+     
+
       return (
             <main className={styles.main}>
+                
                   <Suspense fallback={<p>Loading...</p>}>
                         <Routes>
                               <Route index path={'*'} element={<Navigate to='/profile' replace />} />
@@ -24,7 +30,7 @@ const Main = () => {
                               <Route path='/music' element={<Music />} />
                               <Route path='/settings' element={<Settings />} />
                               <Route path='/users' element={<Users />} />
-                              {/*No render is here*/}
+                              {/* No render is here */}
                               <Route path='/login' element={<Login />} />
                         </Routes>
                   </Suspense>
